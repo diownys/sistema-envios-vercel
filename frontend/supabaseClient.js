72ae0b276757c9904a8f8870fd1b52fd8c5875f6
diownys <diony.r.cordeiro@gmail.com>
2025-12-06 21:19:22 -0300

    Remove Supabase client configuration
    Removed sensitive Supabase configuration details.

--- frontend/supabaseClient.js
+++ frontend/supabaseClient.js
@@ -1,10 +1,1 @@
 // supabaseClient.js
-
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-// CONFIGURAÇÃO CENTRALIZADA
-const SUPABASE_URL = 'https://nfsuisftzddegihyhoha.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mc3Vpc2Z0emRkZWdpaHlob2hhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwMTcwMDcsImV4cCI6MjA3NDU5MzAwN30.tM_9JQo6ejzOBWKQ9XxT54f8NuM6jSoHomF9c_IfEJI';
-
-// Cria e exporta o cliente Supabase
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
